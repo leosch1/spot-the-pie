d597e0762512f19e98d8fa18824815763feee763
leosch1 <35605161+leosch1@users.noreply.github.com>
2021-09-15 18:00:29 +0900

    Add footer

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <template>
   <v-app>
     <v-main class="main">
-      <router-view/>
+      <router-view />
     </v-main>
+    <v-footer class="footer" padless>
+      <span>{{ new Date().getFullYear() }} - Spot The Pie</span>
+    </v-footer>
   </v-app>
 </template>
 
@@ -19,6 +22,10 @@ export default Vue.extend({
 </script>
 
 <style>
+body {
+  background-color: #535353;
+}
+
 .main {
   background-color: #282828;
   color: #ffffff;
@@ -28,5 +35,17 @@ export default Vue.extend({
 .v-application .main * {
   margin: 0;
   padding: 0;
+}
+
+footer.v-footer.footer {
+  background-color: #535353;
+  color: #B3B3B3;
+  font-size: 12px;
+  font-weight: 600;
+  font-family: "Montserrat" !important;
+}
+
+footer span {
+  margin: 0.4rem auto;
 }
 </style>
